6326bfbeb786d01a7be7acba220aa2570238ecc4
Shriyashzzz <ghimires1@my.smccd.edu>
2026-04-01 21:21:33 -0700

    working form reset

--- javascript.js
+++ javascript.js
@@ -11,59 +11,58 @@ function Book(name, author, pages, already_read){
 
 
 
-
+let bookAddForm = document.getElementById(".addBookForm");
 const mainContent = document.querySelector(".mainContent");
-const addBookSubmitbtn = document.querySelector(".submit-form-btn")
 
+addBookSubmitbtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    const book = new Book(
+        document.querySelector("#book-name").value,
+        document.querySelector("#book-author").value,
+        document.querySelector("#book-pages").value,
+        document.querySelector('input[name="finish-book"]:checked')?.value
 
-addBookSubmitbtn.addEventListener("click", function(e){
-    e.preventDefault()
-    let bookName = document.querySelector("#book-name");
-    let authorName = document.querySelector("#book-author")
-    let bookPages = document.querySelector("#book-pages")
-    let bookFinish = document.querySelector('input[name="finish-book"]:checked')
-    addBookTolibrary(bookName.value,authorName.value,bookPages.value,bookFinish.value)
-
-})
-
-
-
-function addBookTolibrary(bookName,authorName,bookPages,bookFinish){  
-    let newBook = new Book(bookName,authorName,bookPages,bookFinish)
-    myLibrary.push(newBook);
-    insertBookInDom(bookName,authorName,bookPages,bookFinish)
-}
-
-
-function insertBookInDom(bookName,authorName,bookPages,bookFinish){
-    let article = document.createElement("article");
-    let h3 = document.createElement('h3');
-    let div = document.createElement("div")
-    let bookNameP = document.createElement('h3');
-    let bookAuthorP = document.createElement('p');
-    const deleteBookBtn = document.createElement("button");
-    let bookPagesP = document.createElement("p")
-    h3.textContent = `${bookName}`;
-    bookAuthorP.textContent= `Author:${authorName}`;
-    deleteBookBtn.classList.add("book-delete-btn")
-    deleteBookBtn.innerText = "Remove"
-    console.log(h3, div)
-    bookPagesP.textContent = `Pages: ${bookPages}`;
+    );
+    addBookToLibrary(book);
+    e.target.parentElement.reset();
    
-    let bookState = document.createElement("p");
+});
 
-    if(bookFinish =="true"){
-        bookState.textContent = `Status: Finished`
-    }else if(bookFinish == "false"){
-        bookState .textContent = `Status: In progress`
-    }
-    bookState.classList.add("bookStateP")
-    div.append(bookNameP, bookAuthorP, bookPagesP, bookState);
-    div.classList.add("bookInfoContainer")
-    article.append(h3,div, deleteBookBtn);
-    article.classList.add("card")
-    mainContent.appendChild(article);
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    insertBookInDom(book);
 }
 
 
 
+
+function insertBookInDom(book) { 
+    const { name, author, pages, already_read } = book;  
+
+    const article = document.createElement("article");
+    const h3 = document.createElement('h3');
+    const div = document.createElement("div");
+    const bookAuthorP = document.createElement('p');
+    const deleteBookBtn = document.createElement("button");
+    const bookPagesP = document.createElement("p");
+    const bookState = document.createElement("p");
+    const svgImg = document.createElement('img');
+
+    h3.textContent = name;
+    bookAuthorP.textContent = `Author: ${author}`;
+    bookPagesP.textContent = `Pages: ${pages}`;
+    bookState.textContent = `Status: ${already_read === "true" ? "Finished" : " Not Finished"}`;
+    bookState.classList.add("bookStateP");
+    svgImg.src = 'icons/delete.svg';
+    svgImg.classList.add("icons", "deleteicon");
+    deleteBookBtn.classList.add("book-delete-btn");
+    deleteBookBtn.appendChild(svgImg);
+    deleteBookBtn.addEventListener('click', () => article.remove());
+
+    div.classList.add("bookInfoContainer");
+    div.append(bookAuthorP, bookPagesP, bookState);
+    article.classList.add("card");
+    article.append(h3, div, deleteBookBtn);
+    mainContent.appendChild(article);
+
+}
